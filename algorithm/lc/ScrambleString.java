bfe736581bd3cb719ce5b93367ca6fd9029a50ac
liangzhaozeng <liangzhao.zeng@gmail.com>
2014-04-06 23:11:43 -0400

    nw

--- algorithm/lc/ScrambleString.java
+++ algorithm/lc/ScrambleString.java
@@ -5,6 +5,33 @@ import java.util.Arrays;
 public class ScrambleString {
   
   public class Solution {
+ 	public boolean isScramble(String s1, String s2) {
+    int n=s1.length();
+        boolean[][][] dp=new boolean[n][n][n+1];
+        
+        for(int i=n-1; i>=0; i--)
+            for(int j=n-1; j>=0; j--)
+                for(int k=1; k<=n-Math.max(i,j);k++)
+                {
+                    if(s1.substring(i,i+k).equals(s2.substring(j,j+k)))
+                        dp[i][j][k]=true;
+                    else
+                    {
+                        for(int l=1; l<k; l++)
+                        {
+                            if ((dp[i][j][l] && dp[i+l][j+l][k-l]) || (dp[i][j+k-l][l] && dp[i+l][j][k-l]))
+                            {
+                                dp[i][j][k]=true;
+                                break;
+                            }
+                        }
+                    }
+                }
+        
+        return dp[0][0][n];
+    }
+
+
     public boolean isScramble(String s1, String s2) {
           // IMPORTANT: Please reset any member data you declared, as
           // the same Solution instance will be reused for each test case.
@@ -54,5 +81,8 @@ public class ScrambleString {
     }
       
   }
+
+
+
 
 }
